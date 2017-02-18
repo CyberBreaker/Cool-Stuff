4a9b6131fc5100a2fda2e5ee9d7889d79e6bf729
CyberBreaker <katil348@gmail.com>
2017-02-18 21:15:52 +0200

    Update Link-Duzenleme-Araci.py

--- Programlar/Link-Duzenleme-Araci.py
+++ Programlar/Link-Duzenleme-Araci.py
@@ -1,8 +1,10 @@
-x = input("Linki girin: \t") #Linkin veya yazını girilmesi istendi.
-z = input("Kaç sayfa yazdırılsın: \t") # Kaç defa yazırılması istendeği soruldu.
+#!/usr/bin/python3
+
+x = input("Linki girin: ") #Linkin veya yazını girilmesi istendi.
+z = input("Kaç sayfa yazdırılsın: ") # Kaç defa yazırılması istendeği soruldu.
 y = int(z) #Kaç defa yazdırılacağı karakter olarak girilmişti tam sayıya çevrildi.
-s = input("Arttırılan sayının sonuna gelecek olan yazı:\t") #Linkin  veya yazının ortasından sayı arttırmak istiyorsanız buraya arttırılan sayıdan sonra gelecek kısımı yazın.
+s = input("Arttırılan sayının sonuna gelecek olan yazı: ") #Linkin  veya yazının ortasından sayı arttırmak istiyorsanız buraya arttırılan sayıdan sonra gelecek kısımı yazın.
 a = 0
 while a < y: # linki girdikten sonra sayfa sayıları sırasıyla arttırıldı.
     a += 1
-    print(x,a ,sep="",end=s +"\n")
+    print(x,a, sep="", end=s + "\n")
